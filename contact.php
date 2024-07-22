bdfe5492b5acf56ae9480e6df85f22af02bba594
Robert Ruelas <bobbyruelas@gmail.com>
2024-07-22 18:18:16 -0500

    postfix change

--- contact.php
+++ contact.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
         $mail->Username   = 'bobbyruelas@gmail.com';               // SMTP username
         $mail->Password   = 'dopme5-xetqid-qehcoX';                  // SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; PHPMailer::ENCRYPTION_SMTPS also accepted
+        $mail->SMTPSecure = Postfix::ENCRYPTION_STARTTLS;         // Enable TLS encryption; PHPMailer::ENCRYPTION_SMTPS also accepted
         $mail->Port       = 587;                                    // TCP port to connect to
     
         
